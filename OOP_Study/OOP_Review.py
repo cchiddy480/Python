bc86bb2228fb458f4000dc90988b330c8992a9c7
cchiddy480 <71733645+cchiddy480@users.noreply.github.com>
2025-03-25 20:44:48 +0000

    Add new Grade class with minimum passing score

--- OOP_Study/OOP_Review.py
+++ OOP_Study/OOP_Review.py
@@ -10,4 +10,7 @@ class Student:
 roger = Student("Roger van der Weyden", "Year 10")
 sandro = Student("Sandro Botticelli", "Year 12")
 pieter = Student("Pieter Bruegel the Elder", "Year 8")
-        
+
+# Creation of new "Grade" class
+class Grade:
+    minimum_passing = 65
