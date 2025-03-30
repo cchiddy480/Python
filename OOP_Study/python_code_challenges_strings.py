4e3c36deb6f573ff12a091823209eddc9b075c95
cchiddy480 <71733645+cchiddy480@users.noreply.github.com>
2025-03-30 18:04:51 +0000

    Fix typo in unique letters comment and add function to count occurrences of a specified character in a string

--- OOP_Study/python_code_challenges_strings.py
+++ OOP_Study/python_code_challenges_strings.py
@@ -1,6 +1,6 @@
 # Challenge 1.
 # Count Letters
-# Count the of uniqie letters that appaear in a string. New letters to be counted, dupiates to be ignroed.
+# Count the of uniqie letters that appaear in a string. New letters to be counted, duplicates to be ignored.
 
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
@@ -11,3 +11,17 @@ def unique_english_letters(word):
             unique_letters.append(letter)
     return len(unique_letters)
 
+# print(unique_english_letters("mississippi"))
+
+# Challenge 2.
+# Count X
+# Count the number of times a letter appears in a string.
+
+def count_char_x(word, x):
+  char_count = 0
+  for letter in word:
+    if letter == x:
+      char_count += 1
+  return char_count
+
+# print(count_char_x("mississippi", "s"))
